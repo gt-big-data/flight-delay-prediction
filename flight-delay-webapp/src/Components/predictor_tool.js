108fefafb815957ce0235559e4fab76ebce0b6d5
Thomas Chin <thomas081904@gmail.com>
2023-03-23 21:41:28 -0500

    edited and created predictor tool component

---
+++ flight-delay-webapp/src/Components/predictor_tool.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+
+export default function predictor_tool() {
+    return (
+        <Stack spacing={2} direction="row">
+            <Button variant="text">Text</Button>
+            <Button variant="contained">Contained</Button>
+            <Button variant="outlined">Outlined</Button>
+        </Stack>
+    )
+}
