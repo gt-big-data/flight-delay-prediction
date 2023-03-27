ef994f9023ee453994cca69d15e3b54c115638af
aishani-chak <113648611+aishani-chak@users.noreply.github.com>
2023-03-27 14:20:10 -0400

    Finished header design

--- flight-delay-webapp/src/App.js
+++ flight-delay-webapp/src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 import PredictorTool from './Components/predictor_tool';
 import HeaderSection from './Components/header_section/header_section';
 
+
 function App() {
   return (
     <div>
-      
       <HeaderSection/>
       <PredictorTool/>
     </div>
--- flight-delay-webapp/src/Components/header_section/header_section.js
+++ flight-delay-webapp/src/Components/header_section/header_section.js
@@ -1,18 +1,119 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import background from "./flightdelaybackground.png";
-//import { blue } from '@mui/material/colors';
+import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+        main: '#578590'
+    },
+    secondary: {
+        main: '#2C3444'
+    },
+    contrastThreshold: 3,
+  },
+});
 
 export default function header_section() {
     return(
-        <Box
-        sx={{
-            width: 1920,
-            height: 1500,
-            //backgroundColor: blue,
-            backgroundImage: `url(${background})`,
-            //opacity: [0.9, 0.8, 0.7],
-        }}
-        />
+        <div>
+            <Box
+            sx={{
+                backgroundColor: '#1F5D6A',
+                backgroundSize: 'cover',
+                position: 'absolute',
+                backgroundRepeat: 'no-repeat',
+                width: '100vw',
+                height: 700,
+                opacity: 0.65,
+                borderBottomLeftRadius: '10%',
+                borderBottomRightRadius: '10%',
+            }}
+            />
+            
+            <Box 
+            component="text"
+            className="font-link-bold"
+            sx={{ 
+                position: 'absolute',
+                paddingTop: 30,
+                paddingLeft: 45,
+                textAlign: 'center',
+                color: 'white',
+                fontSize: 52,
+                fontWeight: 700,
+            }}
+            >Flight Delay Predictor</Box>
+
+            <Box 
+            component="text"
+            className="font-link-reg"
+            sx={{ 
+                width: 650,
+                position: 'absolute',
+                paddingTop: 40,
+                paddingLeft: 38,
+                color: 'white',
+                fontSize: 18,
+                textAlign: 'center',
+            }}
+            >Planning a flight in the next few weeks? Check if your flight is going to be delayed using our tool, which utilizes machine learning models to predict the chances of delay.</Box>
+
+            <ThemeProvider theme={theme}>
+                <Button 
+                variant="contained"
+                color='primary'
+                disableElevation
+                className="font-link-reg"
+                sx={{ 
+                    fontFamily: 'Inter',
+                    fontWeight: 700,
+                    textTransform: 'none',
+                    position: 'absolute',
+                    marginLeft: 59,
+                    marginTop: 55,
+                    color: 'white',
+                }}
+                >Prediction Tool</Button>
+
+                <Button 
+                variant="contained"
+                color='secondary'
+                disableElevation
+                className="font-link-reg"
+                sx={{ 
+                    position: 'absolute',
+                    marginLeft: 80,
+                    marginTop: 55,
+                    color: 'white',
+                    fontFamily: 'Inter',
+                    fontWeight: 700,
+                    textTransform: 'none',
+                }}
+                >Data Visual Tool</Button>
+            </ThemeProvider>
+
+
+            <Box
+            sx={{
+                backgroundImage: `url(${background})`,
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                //position: 'absolute',
+                width: '100vw',
+                height: 700,
+                borderBottomLeftRadius: '10%',
+                borderBottomRightRadius: '10%',
+            }}
+            />
+                
+                
+
+            
+
+        </div>
+        
     );
 }
