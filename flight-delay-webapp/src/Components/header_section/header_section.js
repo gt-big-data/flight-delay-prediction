fa90f1f6f5803d14f0e2f75ed2bd0a15d8c5649b
Thomas Chin <thomas081904@gmail.com>
2023-04-03 20:07:43 -0400

    Made changes to predictor tool formatting and added input function

--- flight-delay-webapp/src/Components/header_section/header_section.js
+++ flight-delay-webapp/src/Components/header_section/header_section.js
@@ -4,89 +4,7 @@ import background from "./flightdelaybackground.png";
 import Button from "@mui/material/Button";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const theme = createTheme({
-<<<<<<< HEAD
-  palette: {
-    primary: {
-      main: "#578590",
-    },
-    secondary: {
-      main: "#2C3444",
-    },
-    contrastThreshold: 3,
-  },
-});
-
-export default function header_section() {
-  return (
-    <div>
-      <Box
-        sx={{
-          backgroundColor: "#1F5D6A",
-          backgroundSize: "cover",
-          position: "absolute",
-          backgroundRepeat: "no-repeat",
-          width: "100vw",
-          height: 700,
-          opacity: 0.65,
-          borderBottomLeftRadius: "10%",
-          borderBottomRightRadius: "10%",
-        }}
-      />
-
-      <Box
-        component="text"
-        className="font-link-bold"
-        sx={{
-          position: "absolute",
-          paddingTop: 30,
-          paddingLeft: 45,
-          textAlign: "center",
-          color: "white",
-          fontSize: 52,
-          fontWeight: 700,
-        }}
-      >
-        Flight Delay Predictor
-      </Box>
-
-      <Box
-        component="text"
-        className="font-link-reg"
-        sx={{
-          width: 650,
-          position: "absolute",
-          paddingTop: 40,
-          paddingLeft: 38,
-          color: "white",
-          fontSize: 18,
-          textAlign: "center",
-        }}
-      >
-        Planning a flight in the next few weeks? Check if your flight is going
-        to be delayed using our tool, which utilizes machine learning models to
-        predict the chances of delay.
-      </Box>
-
-      <ThemeProvider theme={theme}>
-        <Button
-          variant="contained"
-          color="primary"
-          disableElevation
-          className="font-link-reg"
-          sx={{
-            fontFamily: "Inter",
-            fontWeight: 700,
-            textTransform: "none",
-            position: "absolute",
-            marginLeft: 59,
-            marginTop: 55,
-            color: "white",
-          }}
-        >
-          Prediction Tool
-        </Button>
-=======
+export const theme = createTheme({
     palette: {
         primary: {
             main: '#578590'
@@ -198,44 +116,6 @@ export default function header_section() {
                     }}
                 >Data Visual Tool</Button>
             </ThemeProvider>
->>>>>>> frontend
-
-        <Button
-          variant="contained"
-          color="secondary"
-          disableElevation
-          className="font-link-reg"
-          sx={{
-            position: "absolute",
-            marginLeft: 80,
-            marginTop: 55,
-            color: "white",
-            fontFamily: "Inter",
-            fontWeight: 700,
-            textTransform: "none",
-          }}
-        >
-          Data Visual Tool
-        </Button>
-      </ThemeProvider>
-
-<<<<<<< HEAD
-      <Box
-        sx={{
-          backgroundImage: `url(${background})`,
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          //position: 'absolute',
-          width: "100vw",
-          height: 700,
-          borderBottomLeftRadius: "10%",
-          borderBottomRightRadius: "10%",
-        }}
-      />
-    </div>
-  );
-}
-=======
             <Box
                 sx={{
                     backgroundImage: `url(${background})`,
@@ -253,4 +133,3 @@ export default function header_section() {
     );
 }
 
->>>>>>> frontend
--- flight-delay-webapp/src/Components/predictor_tool.js
+++ flight-delay-webapp/src/Components/predictor_tool.js
@@ -1,82 +1,258 @@
-import React from "react";
+import React, {useState} from "react";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import "../App.css";
 import { Box, TextField } from "@mui/material";
 import { fontSize, fontStyle } from "@mui/system";
+import Input from "@mui/material/Input";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import FormControl from "@mui/material/FormControl";
+import Alert from "@mui/material/Alert";
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#578590",
+    },
+    secondary: {
+      main: "#2C3444",
+    },
+    contrastThreshold: 3,
+  },
+});
 
-export default function predictor_tool() {
+export default function Predictor_Tool() {
+  const [flightNumber, setFlightNumber] = useState("UA123");
+  const [showError, setShowError] = useState(false);
+
+  const onSubmit = (e) => {
+    setShowError(true);
+    e.flightNumber = flightNumber;
+    console.log(e.flightNumber);
+    console.log("Submitted");
+  };
   return (
-    <Box sx={{ justifyContent: "center", paddingY: 10 }}>
+    <FormControl
+      onSubmit={onSubmit}
+      sx={{
+        display: "flex",
+        margin: "auto",
+        justifyContent: "center",
+        paddingY: 10,
+      }}
+    >
       <Box>
-        <Stack className="toolTitleBox"
-        //   sx={{
-        //     display: "flex",
-        //     margin: "auto",
-        //     justifyContent: "center",
-        //     alignItems: "center",
-        //     padding: 0,
-        //     width: 800,
-        //     height: 48,
-        //     fontFamily: "Inter",
-        //     fontStyle: "normal",
-        //     fontWeight: 400,
-        //     fontSize: 20,
-        //     textAlign: "center",
-        //     letterSpacing: 0.1,
-        //   }}
-
+        <Box>
+          <Stack className="toolTitleBox">
+            <b>Predictor Tool</b> <br />
+            Enter in the Flight Number and the Departure Destination of your
+            upcoming flight. Then click predict to recieve a percentage of delay
+          </Stack>
+        </Box>
+        <Stack
+          direction="row"
+          sx={{
+            display: "flex",
+            margin: "auto",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 2,
+          }}
         >
-          <b>Predictor Tool</b> <br />
-          Enter in the Flight Number and the Departure Destination of your
-          upcoming flight. Then click predict to recieve a percentage of delay
+          <Stack
+            id="leftDelayedSection"
+            sx={{
+              display: "flex",
+              gap: 2,
+              width: 550, // 493
+              paddingY: 8,
+            }}
+          >
+            <Stack
+              direction="row"
+              id="rowsOfDelay"
+              sx={{
+                display: "flex",
+                gap: 2,
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  padding: 1,
+                  background: "#F7FB39",
+                  border: 1,
+                  borderRadius: 45,
+                  width: 30,
+                  height: 30,
+                }}
+              ></Box>
+              <Box
+                sx={{
+                  fontFamily: "Inter",
+                  fontStyle: "normal",
+                  fontWeight: 600,
+                  fontSize: 25,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                0 - 30 Minutes Delayed
+              </Box>
+              <Box sx={{ border: 1, width: 129 }}></Box>
+            </Stack>
+
+            <Stack
+              direction="row"
+              sx={{
+                display: "flex",
+                gap: 2,
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  padding: 1,
+                  background: "#F7CF43",
+                  border: 1,
+                  borderRadius: 45,
+                  width: 30,
+                  height: 30,
+                }}
+              ></Box>
+              <Box
+                sx={{
+                  fontFamily: "Inter",
+                  fontStyle: "normal",
+                  fontWeight: 600,
+                  fontSize: 25,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                30 - 60 Minutes Delayed
+              </Box>
+              <Box sx={{ border: 1, width: 129 }}></Box>
+            </Stack>
+
+            <Stack
+              direction="row"
+              sx={{
+                display: "flex",
+                gap: 2,
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  padding: 1,
+                  background: "#F77943",
+                  border: 1,
+                  borderRadius: 45,
+                  width: 30,
+                  height: 30,
+                }}
+              ></Box>
+              <Box
+                sx={{
+                  fontFamily: "Inter",
+                  fontStyle: "normal",
+                  fontWeight: 600,
+                  fontSize: 25,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                60+ Minutes Delayed
+              </Box>
+              <Box sx={{ border: 1, width: 129 }}></Box>
+            </Stack>
+          </Stack>
+
+          <Stack
+            sx={{
+              display: "flex",
+              gap: 2,
+              width: 493,
+            }}
+          >
+            <TextField
+              name="flightNumber"
+              id="Flight Number"
+              label="Flight Number*"
+              variant="outlined"
+              onChange={(e) => {setFlightNumber(e.target.value)}}
+              // sx={{
+              //     border: "1px solid #001E6C",
+              //     boxShadow: "inset 4px 4px 8px rgba(0, 0, 0, 0.25)",
+              //     borderRadius: "16px",
+              //     fontFamily: 'Poppins',
+              //     fontStyle: "normal",
+              //     fontWeight: 300,
+              //     color: "#808FB5",
+
+              // }}
+            />
+            <TextField
+              name="departing"
+              id="Departing"
+              label="Departing From*"
+            />
+            <TextField
+              name="arrival"
+              id="Arrival"
+              label="Arriving At*"
+              variant="outlined"
+            />
+          </Stack>
+        </Stack>
+
+        <Stack
+          sx={{
+            display: "flex",
+            margin: "auto",
+            justifyContent: "center",
+            alignItems: "center",
+            width: 484,
+            color: "#229191",
+          }}
+        >
+          <ThemeProvider theme={theme}>
+            <Button
+              type="submit"
+              onClick={(e) => {
+                onSubmit(e);
+              }}
+              variant="contained"
+              color="primary"
+              className="predictButton"
+              sx={{
+                width: 430,
+                height: 60,
+                background: "#229191",
+                borderRadius: 12,
+                fontFamily: "Poppins",
+                fontStyle: "normal",
+                fontWeight: 400,
+                fontSize: 22,
+                letterSpacing: " 0.06em",
+                color: "#FFFFFF",
+              }}
+            >
+              Predict
+            </Button>
+            {showError && (<Alert severity="info" sx={{mt: 2}}> Running the Model... </Alert>)}
+          </ThemeProvider>
         </Stack>
       </Box>
-      <Stack direction="row" sx ={{
-        display: "flex",
-        margin: "auto",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: 2
-      }}>
-        <Stack sx={{
-            display: "flex",
-            gap:2,
-            width: 493,
-            paddingY: 8
-        }}>
-          <TextField id="Flight Number" label="Flight Number*" variant="outlined"
-          />
-          <TextField id="Departing" abel="Departing From*" variant="outlined"
-          />
-        </Stack>
-        <Stack sx={{
-            display: "flex",
-            gap:2,
-            width: 493
-        }}>
-          <TextField id="Flight Number" label="Flight Number*" variant="outlined"
-          />
-          <TextField
-            id="Departing"
-            label="Departing From*"
-            variant="outlined"
-          />
-        </Stack>
-      </Stack>
-
-      <Stack sx={{
-        display: "flex",
-        margin: "auto",
-        justifyContent: "center",
-        alignItems: "center",
-        width: 484,
-        color: '#229191'
-      }}>
-        <Button variant="contained">Predict</Button>
-      </Stack>
-
-    </Box>
-    
+    </FormControl>
   );
 }
